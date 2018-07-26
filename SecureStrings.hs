b0071b7108a54961876f5b2f122df06c976a9a4f
you@example.com <you@example.com>
2018-07-26 14:36:59 +0000

    create mock SecureStrings module

---
+++ SecureStrings.hs
@@ -0,0 +1,18 @@
+
+-- Carnap-Server/SecureStrings.hs
+-- shifted around rev 254
+-- ~ rev 465, material is deleted for Google Pages
+
+module SecureStrings where
+
+import qualified Data.Text as T
+import           Data.Text ( Text )
+
+
+googleApiKey :: Text
+googleApiKey = error "no googleApiKey!!!"
+
+googleSecret :: Text
+googleSecret = error "no googleSecret!!!"
+
+
