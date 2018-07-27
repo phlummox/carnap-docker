14b1b79304d54e5c59ff69fae29e8eb92b1c34fa
phlummox <phlummox@users.noreply.github.com>
2018-07-27 23:32:45 +0800

    EE, part 1

--- mk_ghcjs_symlinks.sh
+++ mk_ghcjs_symlinks.sh
@@ -4,7 +4,8 @@ set -x
 mkdir -p Carnap-Server/static/ghcjs/allactions/
 rm -rf Carnap-Server/static/ghcjs/allactions/*.js
 printf 'removed symlinks\n'
-for file in `ls Carnap-GHCJS/.stack-work/dist/x86_64-linux/*ghcjs/build/AllActions/AllActions.jsexe/{lib,out,runmain}.js`; do
+for file in `ls Carnap-GHCJS/.stack-work/dist/x86_64-linux/*Cabal-1.24*ghcjs/build/AllActions/AllActions.jsexe/{lib,out,runmain}.js`; do
+
   ln -s --relative $file Carnap-Server/static/ghcjs/allactions; 
 done
 printf 'done\n'
