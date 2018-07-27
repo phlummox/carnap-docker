6fb1712157e6ed2c09c16d807e7aae9c39fb93c7
phlummox <phlummox@users.noreply.github.com>
2018-07-27 15:17:34 +0800

    adding tag 10 files

--- mk_ghcjs_symlinks.sh
+++ mk_ghcjs_symlinks.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-
+printf 'regenerating symlinks ...\n'
+set -x
+mkdir -p Carnap-Server/static/ghcjs/allactions/
 rm -rf Carnap-Server/static/ghcjs/allactions/*.js
+printf 'removed symlinks\n'
 for file in `ls Carnap-GHCJS/.stack-work/dist/x86_64-linux/*ghcjs/build/AllActions/AllActions.jsexe/{lib,out,runmain}.js`; do
-  base=`basename $file`;
   ln -s --relative $file Carnap-Server/static/ghcjs/allactions; 
 done
+printf 'done\n'
+
